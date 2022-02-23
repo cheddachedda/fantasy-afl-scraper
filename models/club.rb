356d4c7389ee2c123d2125f28a7180cf844aa9e7
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-02-23 21:33:38 +1100

    Add .find_by_id and .update methods

--- models/club.rb
+++ models/club.rb
@@ -12,6 +12,25 @@ class Club
     db_query(sql).first
   end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM clubs WHERE id=#{id}"
+    db_query(sql).first
+  end
+
+  def self.update(club_id, club_stats)
+    club = find_by_id(club_id)
+
+    club_stats.keys.each do |col|
+      club_stats[col] += club[col.to_s].to_i
+    end
+
+    values = club_stats.map{ |k, v| "#{k.to_s}=#{v}" }.join(', ')
+
+    sql = "UPDATE clubs SET #{values} WHERE id=#{club_id}"
+
+    db_query(sql)
+  end
+
   def initialize(club_hash)
     @club = club_hash
     create

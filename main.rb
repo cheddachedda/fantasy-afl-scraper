24183dd0929d643803df4c8bf2be6bd3f6172238
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-02-24 11:53:34 +1100

    Add runtime logs

--- main.rb
+++ main.rb
@@ -5,9 +5,15 @@ require_relative './src/seed_players.rb'
 require_relative './src/seed_gamelogs.rb'
 require_relative './src/seed_fantasy_values.rb'
 
+start_time = Time.new
+
 seed_clubs
 seed_fixtures
 update_ladder
 seed_players
 seed_gamelogs
 seed_fantasy_values
+
+end_time = Time.new
+run_time = (end_time - start_time).round
+puts "Seeded in #{ run_time } seconds"
